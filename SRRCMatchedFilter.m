329ed17c279ec30be99f3feb4b684bc698c8c619
Alex Christenson <11860900+alch5436@users.noreply.github.com>
2017-12-02 22:23:48 -0500

    Changing some stuff

--- SRRCMatchedFilter.m
+++ SRRCMatchedFilter.m
@@ -1,7 +1,7 @@
 function [SRRC_MF, t2] = SRRCMatchedFilter(alpha, T, K, fs)
     if nargout == 2 
         SRRC_MF = SRRC(alpha, T, K, fs);
-        t2 = linspace(-K*T+T,K*T+T,2*fs*K); %shift in time by KT
+        t2 = linspace(-K*T-T,K*T-T,2*fs*K); %shift in time by KT
     else
         SRRC_MF = SRRC(alpha, T, K, fs);
     end
